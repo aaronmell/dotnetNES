c4f0690f12fb8084f08d55c9ee31ddb527da2a65
Aaron Mell <aaronamell@gmail.com>
2015-01-06 21:34:53 -0600

    Added 2 projects to solution, the client and the engine.

---
+++ sharpNES.Client/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace sharpNES.Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ sharpNES.Client/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace sharpNES.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
